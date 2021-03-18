67d5c99aeac8c7f68c8f6a9dbdbe92ffc28f22c1
JosephCordell <54091637+JosephCordell@users.noreply.github.com>
2021-03-17 18:17:04 -0700

    Gmaps updates with search (#20)

--- assets/script.js
+++ assets/script.js
@@ -3,9 +3,9 @@ const buttonEl = document.getElementById('button')
 const inputEl = document.getElementById('search-text')
 var searchFormEl = document.querySelector('#search-form');
 
-//buttonEl.addEventListener('click', test);
+//buttonEl.addEventListener('click', searchRestaruant);
 
-function test () {
+function searchRestaruant () {
   let input = inputEl.value.trim()
   docuMenuURL = `https://api.documenu.com/v2/restaurants/zip_code/${input}?key=ce2dc71b6458503cfc0e34adfe844c3f`
   fetch(docuMenuURL)
@@ -20,11 +20,20 @@ function test () {
 //console.log(process.env.GOOGLEAPI)
 
 let map;
+let infoWindow;
 
+//creates map that currently centers around Bellevue
 function initMap() {
-  //creates map that currently centers around Bellevue
   map = new google.maps.Map(document.getElementById("map"), {
     center: {lat: 47.6137, lng: -122.19093},
+    zoom: 13,
+  });
+}
+
+//creates map that currently centers around Bellevue
+function updateMap(lati, longi) {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: {lat: lati, lng: longi},
     zoom: 13,
   });
 }
@@ -34,7 +43,7 @@ function businessCards (data) {
   container.innerHTML = ""
   let rlat = data.data[0].geo.lat;
   let rlon = data.data[0].geo.lon;
-
+  updateMap(rlat, rlon)
   console.log(data)
   for (let i = 0; i <= 5; i++) {
         const restaurantCardEl = document.createElement("div"); 
@@ -44,12 +53,18 @@ function businessCards (data) {
         const rAddress = data.data[i].address.formatted;
         let rlat = data.data[i].geo.lat;
         let rlon = data.data[i].geo.lon;
-        console.log(rName); 
         const rInnerHTML = `
         <h1> ${rName} </h1>
         <a href="tel:${rPhone}">${rPhone}</a> <br> 
         <a href="http://maps.google.com?q=${rlat},${rlon} "> Address: ${rAddress}</a>
         `
+
+        //creates an info window, but currently only holds data for the last card, not individual ones. Commented out for now
+/*         infoWindow = new google.maps.InfoWindow({
+          content:`<h1>${rName}</h1> 
+          <p> Phone Number: ${rPhone} </p>
+          <p> Address: ${rAddress} </p>`
+        }) */
         restaurantCardEl.innerHTML = rInnerHTML; 
         container.appendChild(restaurantCardEl);
         addMarker(rlat, rlon)
@@ -63,8 +78,14 @@ function addMarker(latitude, longitude) {
     position: {lat: latitude, lng: longitude},
     map: map
 })
+
+// info window pops up when clicking on the marker
+marker.addListener('click', function () {
+ infoWindow.open(map, marker)
+}) 
 }
 
+//searches a new zip code from the search input
 function handleSearchFormSubmit(event) {
   event.preventDefault();
 
@@ -77,16 +98,10 @@ function handleSearchFormSubmit(event) {
     console.error('You need a search input value!');
     return;
   }
-  test();
+  searchRestaruant();
 }
 
 searchFormEl.addEventListener('submit', handleSearchFormSubmit);
 
-/* UTILIZE FOR POP UP WINDOWS LATER
-  let infoWindow = new google.maps.InfoWindow({
-    content:`<h1>Uwajimaya</h1> <p> Asian food here</p>`
-  })
 
-   marker.addListener('click', function () {
-    infoWindow.open(map, marker)
-  }) */
+
