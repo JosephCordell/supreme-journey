adf6a4a0eff0e21614d47d031395c391170d5747
Joseph Cordell <joseph@jcordell.com>
2021-03-19 14:53:07 -0700

    Updated Cards

--- assets/script.js
+++ assets/script.js
@@ -2,13 +2,19 @@ const container = document.querySelector(".card-container");
 const buttonEl = document.getElementById('button')
 const inputEl = document.getElementById('search-text')
 var searchFormEl = document.querySelector('#search-form');
+var cardEl = document.querySelector('.card')
 
+let zoomLevel = 14;
+let map;
+let infoWindow;
 
-lastWindow=null;
+cardEl.addEventListener('hover', markerBounce);
 
-//buttonEl.addEventListener('click', searchRestaruant);
+function markerBounce() {
+  console.log('BOUNCE')
+}
 
-function searchRestaruant () {
+function searchRestaurant() {
   let input = inputEl.value.trim()
   docuMenuURL = `https://api.documenu.com/v2/restaurants/zip_code/${input}?key=ce2dc71b6458503cfc0e34adfe844c3f`
   fetch(docuMenuURL)
@@ -20,16 +26,12 @@ function searchRestaruant () {
   });
 };
 
-//console.log(process.env.GOOGLEAPI)
-
-let map;
-let infoWindow;
 
 //creates map that currently centers around Bellevue
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     center: {lat: 47.6137, lng: -122.19093},
-    zoom: 13,
+    zoom: zoomLevel,
   });
 }
 
@@ -37,7 +39,7 @@ function initMap() {
 function updateMap(lati, longi) {
   map = new google.maps.Map(document.getElementById("map"), {
     center: {lat: lati, lng: longi},
-    zoom: 13,
+    zoom: zoomLevel,
   });
 }
 
@@ -48,22 +50,41 @@ function businessCards (data) {
   let rlon = data.data[0].geo.lon;
   updateMap(rlat, rlon)
   console.log(data)
-  for (let i = 0; i <= 5; i++) {
+  for (let i = 0; i <= data.data.length; i++) {
         const restaurantCardEl = document.createElement("div"); 
         restaurantCardEl.classList.add("card"); 
         const rName = data.data[i].restaurant_name; 
         const rPhone = data.data[i].restaurant_phone;
         const rAddress = data.data[i].address.formatted;
+        const rWebsite = data.data[i].restaurant_website;
         let rlat = data.data[i].geo.lat;
         let rlon = data.data[i].geo.lon;
-        const rInnerHTML = `
-        <h1> ${rName} </h1>
-        <a href="tel:${rPhone}">${rPhone}</a> <br> 
-        <a href="http://maps.google.com?q=${rlat},${rlon} "> Address: ${rAddress}</a>
-        `
-        restaurantCardEl.innerHTML = rInnerHTML; 
-        container.appendChild(restaurantCardEl);
-        addMarker(rlat, rlon, rInnerHTML)
+        let rCuisines;
+        if (data.data[i].cuisines[0] != '') {
+            rCuisines = data.data[i].cuisines;
+            const rInnerHTML = `
+            <h1> ${rName} </h1> <br>
+            Type of food: ${rCuisines[0]} <br>
+            Phone Number: <a href="tel:${rPhone}">${rPhone}</a> <br> 
+            Address: <a href="http://maps.google.com?q=${rlat},${rlon} ">${rAddress}</a> <br>
+            Website Link: <a href="${rWebsite}" target=”_blank”>Website</a>
+            `
+            restaurantCardEl.innerHTML = rInnerHTML; 
+            container.appendChild(restaurantCardEl);
+            addMarker(rlat, rlon, rInnerHTML)
+        } else {
+          const rInnerHTML = `
+          <h1> ${rName} </h1> <br>
+          Type of food: Unknown<br>
+          Phone Number: <a href="tel:${rPhone}">${rPhone}</a> <br> 
+          Address: <a href="http://maps.google.com?q=${rlat},${rlon} ">${rAddress}</a> <br>
+          Website Link: <a href="${rWebsite}" target=”_blank”>Website</a>
+          `
+          restaurantCardEl.innerHTML = rInnerHTML; 
+          container.appendChild(restaurantCardEl);
+          addMarker(rlat, rlon, rInnerHTML)
+        }
+
 }
 }
 
@@ -72,12 +93,19 @@ function addMarker(latitude, longitude, note) {
   let marker = new google.maps.Marker({
     map: map,
     position: {lat: latitude, lng: longitude},
+    animation: google.maps.Animation.DROP,
     clickable: true
   })
   google.maps.event.addListener(marker, 'click', function() {
     infoWindow.setContent(note);
     infoWindow.open(map, marker);
+    if (marker.getAnimation() !== null) {
+      marker.setAnimation(null);
+    } else {
+      marker.setAnimation(google.maps.Animation.BOUNCE);
+    }
   });
+
 }
 
 
@@ -100,7 +128,7 @@ function handleSearchFormSubmit(event) {
     console.error('You need a search input value!');
     return;
   }
-  searchRestaruant();
+  searchRestaurant();
 }
 
 searchFormEl.addEventListener('submit', handleSearchFormSubmit);
