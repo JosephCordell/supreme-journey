dbc1670f9e641be861c91a1051aef378e1d71a64
melissa-tan <melissatan9683@gmail.com>
2021-03-21 16:07:21 -0700

    fixed localStorage issue v2

--- assets/main.js
+++ assets/main.js
@@ -1,21 +1,61 @@
 var searchFormEl = document.querySelector('#search-form');
+const getHistory = document.querySelector("#get-history");
+const clearHistoryBtnEl = document.querySelector(".clear-history");
+clearHistoryBtnEl.addEventListener("click", clearHistory);
 
 function handleSearchFormSubmit(event) {
   event.preventDefault();
 
   var searchTextVal = document.querySelector('#search-text').value;
 
-  //use below if we have search options dropdown
-/*   var formatInputVal = document.querySelector('#format-input').value; */
-
   if (!searchTextVal) {
     console.error('You need a search input value!');
     return;
   }
 
-  var queryString = './search-results.html?q=' + searchTextVal/*  + '&format=' + formatInputVal */;
+  var queryString = './search-results.html?q=' + searchTextVal;
 
   location.assign(queryString);
+  updateHistory(searchTextVal);
+
 }
 
 searchFormEl.addEventListener('submit', handleSearchFormSubmit);
+
+function updateHistory(userZip){
+  document.querySelector("#get-history").textContent = "";
+  let storage = JSON.parse(localStorage.getItem("userInput"));
+  let zipHistory=[userZip];
+  if(storage != null){
+      storage.forEach(zip=>{
+          if (zip !== userZip){
+            zipHistory.push(zip);
+          }
+      })
+  }
+    var jsonObject = JSON.stringify(zipHistory);
+    localStorage.setItem("userInput", jsonObject);
+    
+  }
+  
+  function viewHistory(){
+    getHistory.innerHTML="";
+    var storage = JSON.parse(localStorage.getItem("userInput"));
+    console.log(storage)
+    for (let i = 0; i < storage.length; i++) {      
+      let displayHistory = document.createElement("div");
+      let updateItem =
+      `<button class="history-item btn" data-id="${storage[i]}"onclick="handleSearchFormSubmit(event)">${storage[i]}</button>`;
+      displayHistory.innerHTML = updateItem;
+      getHistory.appendChild(displayHistory);
+    }
+      
+}
+
+function clearHistory(){
+  localStorage.clear("userInput");
+  localStorage.removeItem("userInput");
+  getHistory.innerHTML="";
+}
+
+viewHistory();
